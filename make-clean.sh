7650dc74e3a29b0c541e8e5a99ab9969a5d1d68b
Manuel de Prada <manudroid19@users.noreply.github.com>
2019-06-06 16:23:26 +0200

    Update make-clean.sh

--- make-clean.sh
+++ make-clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed '/BEGIN/d' Apuntes.tex > temp.tex
-latexmk -pdf temp.tex
-mv temp.pdf Apuntes_clean.pdf
+sed '/BEGIN/d' Apuntes.tex > temp.tex #eliminar lineas que empiezan por BEGIN
+latexmk -pdf temp.tex #compilar a pdf
+mv temp.pdf Apuntes_clean.pdf #limpiar
 rm temp.*
